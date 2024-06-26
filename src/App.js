4633e013ca5028233b5bc36d6716d87d22fca463
mquispe96 <mquispe@pursuit.org>
2024-06-26 10:57:28 -0400

    Setup routes and create main components files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>Hello World</div>;
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/NavBar";
+import Logs from "./Components/Logs";
+import ShowLog from "./Components/ShowLog";
+import NewLog from "./Components/NewLog";
+import EditLog from "./Components/EditLog";
+
+const App = () => {
+  return(
+    <Routes>
+      <Route path='/' element={<Navbar />}>
+        <Route index element={<Logs />} />
+        <Route path='show/:id' element={<ShowLog />} />
+        <Route path='create' element={<NewLog />} />
+        <Route path='edit/:id' element={<EditLog />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App;
