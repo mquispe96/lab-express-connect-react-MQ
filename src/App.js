57cbba242d5b4deaddb4f0c59a512f2f8a8b42fa
mquispe96 <mquispe@pursuit.org>
2024-06-27 15:49:07 -0400

    Added Navigate component to redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Navigate} from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Logs from './Components/Logs';
 import ShowLog from './Components/ShowLog';
@@ -8,6 +8,7 @@ import EditLog from './Components/EditLog';
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/logs" replace/>} />
       <Route path="/logs" element={<NavBar />}>
         <Route index element={<Logs />} />
         <Route path="/logs/new" element={<NewLog />} />
